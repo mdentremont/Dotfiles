67de0ed41656b89411d42e786240fd089a472f3b
Matt d'Entremont <mattdentremont@gmail.com>
2022-04-22 23:21:54 -0300

    WIP: dotfiles2

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+if test -f /opt/dev/dev.fish
+  source /opt/dev/dev.fish
+end
+
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
