2177bad4693e83a0d4bebbf7bf08a235ec62f72d
Matt d'Entremont <matt.dentremont@shopify.com>
2025-02-11 11:03:03 -0400

    Configure prettier to work in nvim

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -5,3 +5,7 @@ vim.g.mapleader = ","
 
 local opt = vim.opt
 opt.scrolloff = 20
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = true
