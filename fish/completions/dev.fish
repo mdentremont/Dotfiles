b4a2ccede55fd19a8cbe99bd9ce0f22cae2b0887
Matt d'Entremont <matt.dentremont@shopify.com>
2026-01-22 11:25:47 -0400

    Don't need dev completions anymore they are builtin

--- fish/completions/dev.fish
+++
@@ -1,7 +0,0 @@
-function __fish_dev_trees
-    ls -1 ~/world/trees/ 2>/dev/null
-end
-
-# Complete tree names for dev cd -t
-complete -c dev -n '__fish_seen_subcommand_from cd' -s t -xa '(__fish_dev_trees)'
-
