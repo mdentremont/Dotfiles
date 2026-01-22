73c6c75295750bf8d4238d5a2d4742f8b8edd25d
Matt d'Entremont <matt.dentremont@shopify.com>
2026-01-22 11:22:46 -0400

    Remove fzf fish stuff
    - It got messy and I wasn't using it

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,11 +1,26 @@
 # Astronaut prompt
 function fish_prompt --description 'Write out the prompt'
     set -l last_status $status
+    set -l last_duration $CMD_DURATION
     set -l normal (set_color normal)
     set -l status_color (set_color brgreen)
     set -l cwd_color (set_color $fish_color_cwd)
     set -l vcs_color (set_color brpurple)
+    set -l duration_color (set_color bryellow)
     set -l prompt_status ""
+    set -l prompt_duration ""
+
+    # Show duration for commands that took longer than 1 second
+    if test $last_duration -gt 1000
+        set -l seconds (math --scale=1 $last_duration / 1000)
+        if test $seconds -ge 60
+            set -l minutes (math --scale=0 $seconds / 60)
+            set -l remaining_seconds (math --scale=0 $seconds % 60)
+            set prompt_duration $duration_color " " $minutes m $remaining_seconds s $normal
+        else
+            set prompt_duration $duration_color " " $seconds s $normal
+        end
+    end
 
     # Since we display the prompt on a new line allow the directory names to be longer.
     set -q fish_prompt_pwd_dir_length
@@ -26,6 +41,6 @@ function fish_prompt --description 'Write out the prompt'
         set prompt_status $status_color "[" $last_status "]" $normal
     end
 
-    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal $prompt_duration ' ' $prompt_status
     echo -n -s $status_color $suffix ' ' $normal
 end
